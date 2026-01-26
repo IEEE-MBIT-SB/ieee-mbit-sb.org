d034e5fe6fbe8d70703a04a5c2daf0f2171578bc
YASMIN <yasmin0sekh0@gmail.com>
2026-01-26 19:19:52 +0530

    Add missing ScrollToTop component 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import ScrollToTop from "@/components/ScrolltoTop";
-// ...existing code...
-import ScrollToTop from "@/components/ScrolltoTop";
-// ...existing code...
 
 import Index from "./pages/Index";
 import About from "./pages/About";
